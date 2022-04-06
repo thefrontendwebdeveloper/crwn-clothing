1080667108a55f568a0a25fee42350ac70d02cc3
thefrontendwebdeveloper <himangshu.web.design@gmail.com>
2022-04-06 17:12:55 +0530

    updated with collection-item and collection-preview components

--- src/App.js
+++ src/App.js
@@ -5,18 +5,14 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import ShopPage from './pages/shop-page/shop-page';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
